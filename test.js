4e29c24cb23c8526774ae0623866a40f828abbe3
David Lee <68622371+davvidlee@users.noreply.github.com>
2022-10-27 13:26:48 -0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,8 @@
+var team1 = {1: {name: 'Palkia', moves: ['Hydro Pump', 'Spacial Rend', 'Earth Power', 'Trick Room']}
+            ,2: {name: 'Amoonguss', moves: ['Pollen Puff', 'Rage Powder', 'Spore', 'Protect']}
+            ,3: {name: 'Calyrex-Ice', moves:['Glacial Lance', 'High Horsepower', 'Swords Dance', 'Trick Room']}
+            ,4: {name: 'Incineroar', moves:['Flare Blitz', 'Darkest Lariat', 'Fake Out', 'Parting Shot']}
+            ,5: {name: 'Porygon2', moves:['Foul Play', 'Recover', 'Eerie Impulse', 'Trick Room']}
+            ,6: {name: 'Tapu Fini', moves:['Moonblast', "Nature's Madness", 'Heal Pulse', 'Protect']}};
+
+console.log(Math.floor(Math.random() * 1))
